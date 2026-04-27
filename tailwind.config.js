e8bc2d63305e51f73735ed721a4f3f7c2cb0604a
gozdziovsky <gozdziovsky@gmail.com>
2026-04-27 13:55:15 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", // To mówi Tailwindowi: "Zajrzyj tutaj i znajdź klasy!"
   ],
   theme: {
     extend: {},
